4e3026620e65d81d196020b7817ba7c5d9f8f28c
Narayan <privatejava@yahoo.com>
2022-01-29 02:13:12 +0545

    added pull script

---
+++ pull.sh
@@ -0,0 +1,17 @@
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/apps-akka
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/freeswitch
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/fsesl-akka
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/html5
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/jodconverter
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/recordings
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/web
+docker pull 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/webrtc-sfu
+
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/apps-akka docker_apps-akka
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/freeswitch docker_freeswitch
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/fsesl-akka docker_fsesl-akka
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/html5 bbb-html5
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/jodconverter docker_jodconverter
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/recordings docker_recordings
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/web docker_bbb-web
+docker tag 309168395754.dkr.ecr.ap-southeast-1.amazonaws.com/bbb/webrtc-sfu docker_webrtc-sfu
